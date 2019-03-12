a7ed797c797d8d4dc8e6908ecf0ea716e47e843d
ghatolamol <ahghatol@gmail.com>
2019-03-12 15:33:42 +0530

    Issue #141189 chore: TJQueue Deployment script and improvements

--- scripts/pipelines/nightly.groovy
+++ scripts/pipelines/nightly.groovy
@@ -3,7 +3,7 @@
 // Get / Set release name
 // TODO - remove hardcoded value
 def  tjqueueVersion = '0.0.1' //env.getProperty("tjqueueVersion")
-echo tjqueueVersion
+//echo tjqueueVersion
 
 pipeline {
     agent any
@@ -12,16 +12,10 @@ pipeline {
             steps {
                 script {
                     // Cleanup previous stuff
-                    sh("rm -rf tjqueue-scm")
-                    sh("rm -rf builds")
-
-                    // Cleanup tjqueue git folder, files
-                    sh("rm -rf com_tjqueue")
-                    sh("rm -rf .git")
-                    sh("rm -rf .gitlab/merge_request_templates")
-                    sh("rm -rf build")
-                    sh("rm -rf scripts")
-                    sh("rm -f .gitignore")
+                    def cleanUpList =["tjqueue-scm","builds","com_tjqueue",".git",".gitlab/merge_request_templates","build","scripts", ".gitignore","sqs","dbal"] as String[]
+                    for (item in cleanUpList) {
+                       sh("cd builds/com_tjqueue/media/libs/vendor/aws/aws-sdk-php/src && rm -rf "+item);
+                    }
 
                     // Make directories needed to generate build
                     sh("mkdir builds")
@@ -49,13 +43,17 @@ pipeline {
                     // @TODO - move this to json itself?
                     def subextensions = ['com_tjqueue']
 
-                    // def props = readJSON text: '{}' // Add JSON here or specify file below
+                    def props = readJSON text: '{}' // Add JSON here or specify file below
                     def props = readJSON file: 'tjqueue-scm/build/version.json'
 
                     subextensions.eachWithIndex { item, index ->
                        // Do clone all subextensions repos by checking out corresponding release branch
                        sh("git clone --branch " + props['tjqueue'][tjqueueVersion][item]['branch'] + " --depth 1 " + props['tjqueue'][tjqueueVersion][item]['repoUrl'])
                     }
+
+                    //sh("git clone --branch master --depth 1 https://github.com/ahghatol/com_tjqueue.git")
+                    //sh("git clone --branch 0.9.8 --depth 1 https://github.com/php-enqueue/dbal.git")
+                    //sh("git clone --branch 0.9.8 --depth 1 https://github.com/php-enqueue/sqs.git")
                 }
             }
         }
@@ -65,10 +63,37 @@ pipeline {
                 script {
                     // Copy com_tjqueue from tjqueue repo into builds folder
                     sh("cp -r com_tjqueue/src/* builds/")
+                    sh("rsync -a dbal/ builds/com_tjqueue/media/libs/")
+                    sh("rsync -a sqs/ builds/com_tjqueue/media/libs/")
+                    sh("mv builds/com_tjqueue/media/libs/composer_files/* builds/com_tjqueue/media/libs/")
+                    sh("rm -rf builds/com_tjqueue/media/libs/composer_files")
                 }
             }
         }
 
+       stage('composer') {
+           steps {
+               script {
+                   sh("cd builds/com_tjqueue/media/libs/ && composer install --no-dev --ignore-platform-reqs")
+                    def cleanUpList = [".git","tests","docs","Tests"] as String[]
+                    def deleteCmd = '-exec rm -r "{}" +';
+                    for (item in cleanUpList) {
+                       sh("cd builds/com_tjqueue/media/libs/ && find . -name "+item+" "+ deleteCmd);
+                    }
+                    
+                    def awsCleanUp =["Acm","CostExplorer","IoT1ClickProjects","Rds","ACMPCA","IoTAnalytics","RDSDataService","AlexaForBusiness","Crypto","IotDataPlane","Redshift","Amplify","IoTJobsDataPlane","Rekognition","DatabaseMigrationService","Kafka","ResourceGroups","ApiGateway","DataPipeline","Kinesis","ResourceGroupsTaggingAPI","ApiGatewayManagementApi","DataSync","KinesisAnalytics","RoboMaker","ApiGatewayV2","DAX","KinesisAnalyticsV2","Route53","ApplicationAutoScaling","DeviceFarm","KinesisVideo","Route53Domains","ApplicationDiscoveryService","DirectConnect","KinesisVideoArchivedMedia","Route53Resolver","AppMesh","DirectoryService","KinesisVideoMedia","S3","Appstream","DLM","Kms","S3Control","AppSync","DocDB","Lambda","SageMaker","Athena","DynamoDb","LexModelBuildingService","SageMakerRuntime","AutoScaling","DynamoDbStreams","LexRuntimeService","SecretsManager","AutoScalingPlans","Ec2","LicenseManager","SecurityHub","Backup","Ecr","Lightsail","ServerlessApplicationRepository","Batch","Ecs","MachineLearning","ServiceCatalog","Budgets","Efs","Macie","ServiceDiscovery","Chime","EKS","MarketplaceCommerceAnalytics","Ses","ElastiCache","MarketplaceEntitlementService","Sfn","Cloud9","ElasticBeanstalk","MarketplaceMetering","Shield","CloudDirectory","ElasticLoadBalancing","MediaConnect","CloudFormation","ElasticLoadBalancingV2","MediaConvert","signer","CloudFront","ElasticsearchService","MediaLive","Sms","CloudHsm","ElasticTranscoder","MediaPackage","SnowBall","CloudHSMV2","Emr","MediaStore","Sns","CloudSearch","MediaStoreData","CloudSearchDomain","MediaTailor","Ssm","CloudTrail","Exception","MigrationHub","StorageGateway","CloudWatch","Firehose","Mobile","Sts","CloudWatchEvents","FMS","MQ","Support","CloudWatchLogs","FSx","MTurk","Swf","CodeBuild","GameLift","Multipart","Textract","CodeCommit","Glacier","Neptune","TranscribeService","CodeDeploy","GlobalAccelerator","OpsWorks","Transfer","CodePipeline","Glue","OpsWorksCM","Translate","CodeStar","Greengrass","Organizations","Waf","CognitoIdentity","GuardDuty","PI","WafRegional","CognitoIdentityProvider","Pinpoint","WorkDocs","CognitoSync","Health","PinpointEmail","WorkLink","Comprehend","Iam","PinpointSMSVoice","WorkMail","ComprehendMedical","ImportExport","Polly","WorkSpaces","ConfigService","Inspector","Pricing","XRay","Connect","Iot","QuickSight","CostandUsageReportService","IoT1ClickDevicesService","RAM"] as String[]
+                    for (item in awsCleanUp) {
+                       sh("cd builds/com_tjqueue/media/libs/vendor/aws/aws-sdk-php/src && rm -rf "+item);
+                    }
+                    
+                    def awsDataClenaup=["acm","cognito-sync","fsx","mediapackage","sagemaker","acm-pca","comprehend","gamelift","mediastore","secretsmanager","alexaforbusiness","comprehendmedical","glacier","mediastore-data","securityhub","amplify","config","globalaccelerator","mediatailor","serverlessrepo","apigateway","connect","glue","metering.marketplace","servicecatalog","apigatewaymanagementapi","cur","greengrass","mgh","servicediscovery","apigatewayv2","data.iot","guardduty","mobile","shield","application-autoscaling","datapipeline","health","monitoring","signer","appmesh","datasync","iam","mq","sms","appstream","dax","importexport","mturk-requester","sms-voice","appsync","devicefarm","inspector","neptune","snowball","athena","directconnect","iot","opsworks","sns","autoscaling","discovery","iot1click-devices","opsworkscm","autoscaling-plans","dlm","iot1click-projects","organizations","ssm","backup","dms","iotanalytics","pi","states","batch","docdb","iot-jobs-data","pinpoint","storagegateway","budgets","ds","kafka","pinpoint-email","streams.dynamodb","ce","dynamodb","kinesis","polly","sts","chime","ec2","kinesisanalytics","pricing","support","cloud9","ecr","kinesisanalyticsv2","quicksight","swf","clouddirectory","ecs","kinesisvideo","ram","textract","cloudformation","eks","kinesis-video-archived-media","rds","transcribe","cloudfront","elasticache","kinesis-video-media","rds-data","transfer","cloudhsm","elasticbeanstalk","kms","redshift","translate","cloudhsmv2","elasticfilesystem","lambda","rekognition","waf","cloudsearch","elasticloadbalancing","lex-models","resource-groups","waf-regional","cloudsearchdomain","elasticloadbalancingv2","license-manager","resourcegroupstaggingapi","workdocs","cloudtrail","elasticmapreduce","lightsail","robomaker","worklink","codebuild","elastictranscoder","logs","route53","workmail","codecommit","email","machinelearning","route53domains","workspaces","codedeploy","entitlement.marketplace","macie","route53resolver","xray","codepipeline","es","marketplacecommerceanalytics","runtime.lex","codestar","events","mediaconnect","runtime.sagemaker","cognito-identity","firehose","mediaconvert","s3","cognito-idp","fms","medialive","s3control"] as String[]
+                    for (item in awsDataClenaup) {
+                       sh("cd builds/com_tjqueue/media/libs/vendor/aws/aws-sdk-php/src/data && rm -rf "+item);
+                    }
+               }
+           }
+       }
+       
         stage('Make zips') {
             steps {
                 script {
@@ -88,10 +113,12 @@ pipeline {
                     }
 
                     // Now we are good to create zip for component
-                    dir('builds/com_tjqueue') {
+                    dir('builds') {
                         sh('zip -rq com_tjqueue.zip com_tjqueue')
+                        sh('rm -rf com_tjqueue')
                         sh('zip -rq core.zip core')
-                        sh('zip -rq ../com_tjqueue_v' + tjqueueVersion + '_' + shortGitCommit + '.zip .')
+                        sh('rm -rf core')
+                        sh('zip -rq --exclude=com_tjqueue ../pkg_tjqueue_v' + tjqueueVersion + '_' + shortGitCommit + '.zip .')
                     }
                 }
             }
@@ -115,11 +142,7 @@ pipeline {
                         echo shortGitCommit
                     }
 
-                    // Change to builds directory
-                    dir('builds') {
-                        // Archive Artifact
-                        archiveArtifacts 'com_tjqueue_v' + tjqueueVersion + '_' + shortGitCommit + '.zip'
-                    }
+                    archiveArtifacts 'pkg_tjqueue_v' + tjqueueVersion + '_' + shortGitCommit + '.zip'
                 }
             }
         }
@@ -128,7 +151,7 @@ pipeline {
             steps {
                 script {
                     // Cleanup, so next time we get fresh files
-                    sh("rm -r builds/com_tjqueue/")
+                    sh("rm -r builds")
                 }
             }
         }
